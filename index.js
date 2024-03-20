0050fa076e468c25eda4ed4408d937dc5264e02b
ashok <asknitharwal@gmail.com>
2024-03-20 17:48:01 +0530

    task complete

---
+++ index.js
@@ -0,0 +1,21 @@
+let a= 10;
+let b =5;
+let c= 2;
+ a>b && a>c ? console.log("a is greater") : b>a && b>c ? console.log("b is greater") : console.log("c is greater")
+
+//  *******************************************************************************************************************
+
+let num= -1;
+num>0 ? console.log("Positive Number") : console.log("Negative Number")
+
+// *****************************************************************************************************************************
+
+let isloggedIn= false;
+isloggedIn==true ? console.log("Welcome back!") : console.log("Please log in")
+
+// ********************************************************************************************************************
+
+let dayOfWeek= "sunday";
+ dayOfWeek=="saturday" || dayOfWeek=="sunday" ? console.log("Weekend") : console.log("not weekend") 
+
+//  **************************************************************************************************************************
